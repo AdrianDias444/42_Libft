928f36ab801209cd80cd8ee7893c4b12fb0160d5
AdrianDias444 <adriandias0709@github.com>
2025-10-03 15:22:02 +0100

    ft_strlcpy done

---
+++ libft/functions/str/ft_strlcpy.c
@@ -0,0 +1,27 @@
+size_t ft_strlen(const char *s)
+{
+    size_t i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    size_t i;
+    size_t src_len;
+
+    src_len = ft_strlen(src);
+    i = 0;
+    while (src[i] && i < (size - 1) && size > 0)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (src_len);
+}
