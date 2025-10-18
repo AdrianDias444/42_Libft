616d9f1d002375c40bc6a0674954faa69ecea455
AdrianDias444 <adriandias0709@gmail.com>
2025-10-18 12:15:56 +0100

    ft_memcpy done

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+    size_t i;
+
+    if(!dest && !src)
+        return(NULL);
+
+    i = 0;
+    while(i < n)
+    {
+        ((char *)dest)[i] = ((const char*)src)[i];
+        i++;
+    }
+    return(dest);
+}
+
+/*
+int main(void)
+{
+
+    int a = 4;
+    int b = 2;
+
+    printf("O valor inicial de b: %d\n", b);
+    ft_memcpy(&b, &a, sizeof(int));
+    printf("O valor atual de b: %d\n", b);
+}
+*/
